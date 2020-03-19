79260e2f5e79551baf6018b71ca414a375981a5e
Samuel P. M <samuelmeneses800@gmail.com>
2020-03-19 20:06:03 -0300

    Add ExerciciosDoMoodle

---
+++ ExerciciosDoMoodle/src/exerciciosdomoodle/PCFP/listra1/Exercicio5.java
@@ -0,0 +1,17 @@
+
+package exerciciosdomoodle.PCFP.listra1;
+
+import java.util.Scanner;
+
+
+public class Exercicio5 {
+public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Ensira a aprimeira nota");
+    int a = in.nextInt();
+    System.out.println("Ensira a segunda nota");
+    int b = in.nextInt();
+    System.out.println("A media anual é "+ (a+b)/2);
+    
+}    
+}
---
+++ ExerciciosDoMoodle/src/exerciciosdomoodle/PCFP/listra1/Exercicio7.java
@@ -0,0 +1,15 @@
+
+package exerciciosdomoodle.PCFP.listra1;
+
+import java.util.Scanner;
+
+
+public class Exercicio7 {
+   public static void main(String [] args){
+      System.out.println("Esnsira dois numeros para serem divididos");
+      Scanner in = new Scanner(System.in);
+      int a = in.nextInt();
+      int l = in.nextInt();
+      System.out.println(a / l);
+   }
+}
